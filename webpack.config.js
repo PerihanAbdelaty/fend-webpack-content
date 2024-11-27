b258b61f62ce36bda736321e04879d259467f0f1
PerihanMAbdelaty <perry@dkilo.com>
2024-11-27 13:58:02 +0200

    plugin installation

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
-const path = require('path')
-const webpack = require('webpack')
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [new ESLintPlugin(options)],
-}
-
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
