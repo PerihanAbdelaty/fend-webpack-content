3f1c94be3b59e82df28333cc4e9db807ccd54b46
PerihanMAbdelaty <perry@dkilo.com>
2024-11-27 15:38:14 +0200

    webapck optimizations

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,13 @@ const path = require("path");
 const webpack = require("webpack");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devtool: "source-map",
   entry: "./src/client/index.js",
   module: {
     rules: [
@@ -20,6 +23,18 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
     }),
   ],
 };
